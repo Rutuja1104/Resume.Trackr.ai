79619fbc8fbcc31886a6c1cb537d9ef670a36b44
Rutuja1104 <rutujasahare2018@gmail.com>
2025-10-11 21:38:54 +0530

    Added changes for home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Resumind" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
+      <Navbar/>
+      <section className={"main-section"} >
+        <div className={"page-heading"}>
+            <h1>Track Your Applications & resume Ratings</h1>
+            <h2>Review Your submissions and check AI-powered feedback</h2>
+        </div>
+      {
+          resumes?.length && (
+              <div className={"resumes-section"}>
+                  {resumes.map((resume)=>{
+                      return (
+                          <ResumeCard key ={resume.id} resume={resume}/>
+                      )
+                  })}
+              </div>
+          )
+      }
+      </section>
+
+  </main>
 }
