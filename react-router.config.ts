0c66f99d81dc9d4695382a8a27e36dd63aac7d50
Rutuja1104 <rutujasahare2018@gmail.com>
2025-11-29 15:25:16 +0530

    added changes gh pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename: "/Resume.Trackr.ai",   // ✅ IMPORTANT FIX
   ssr: false,
 } satisfies Config;
